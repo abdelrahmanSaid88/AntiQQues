e1ff367cb4eef5bbc4429f81d60d8eef78515488
abdelrahmanSaid88 <biboosaid1@gmail.com>
2020-06-01 15:29:02 +0200

    First load

---
+++ app/src/main/java/com/example/antiques/Intro1.java
@@ -0,0 +1,14 @@
+package com.example.antiques;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro1);
+    }
+}
